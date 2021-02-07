24a767068969519044bf7388a721f457190f118c
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-02-07 08:42:11 -0500

    commit tree bc76e716bc07b899de1c3cbbd7b6974ce1bae112

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
-# Add your code here
+require 'pry'
+
+class Dog
+ 
+    @@all = []
+
+    attr_accessor :name, :save
+
+    def initialize(name)
+        @name = name
+        save
+    end    
+
+    def self.all
+        @@all
+    end  
+    
+    def self.print_all
+       puts self.all.map {|dog| dog.name}
+      
+    end
+    
+    def self.clear_all
+        self.all.clear
+    end    
+
+    def save
+        @@all << self
+
+        #binding.pry
+    end    
+end
+
+dog1 = Dog.new("pet")
+dog2 = Dog.new("pop")
+
+# binding.pry
+# 0
